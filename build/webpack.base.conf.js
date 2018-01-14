f64fcbbc1244970f7ab2d83da1723229d35becb2
Dylan <botao@babyeye.com>
2018-01-14 14:27:39 +0800

    搭建骨架

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -35,6 +35,8 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       '@': resolve('src'),
+      'src': resolve('src'),
+      'common': resolve('src/common')
     }
   },
   module: {
